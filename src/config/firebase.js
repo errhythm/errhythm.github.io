763de171f7cb060d424f72d800fc650f4a0706d8
Ehsanur Rahman Rhythm <errhythm.me@gmail.com>
2024-10-14 15:32:00 +0600

    Refactor .gitignore to exclude environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBnve-GV2W77GgG2uPLkV7pYI7pl7JaYp0',
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
   authDomain: 'errhythm-gatsby-stats.firebaseapp.com',
   databaseURL: 'https://errhythm-gatsby-stats-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'errhythm-gatsby-stats',
   storageBucket: 'errhythm-gatsby-stats.appspot.com',
-  messagingSenderId: '815067165388',
-  appId: '1:815067165388:web:950a3c92e5ea1c89230869',
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
