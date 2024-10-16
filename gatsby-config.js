a7aa5102d5971739c8600a96c461c5f43babfc94
Ehsanur Rahman Rhythm <errhythm.me@gmail.com>
2024-10-16 23:11:02 +0600

    feat: Add gatsby-plugin-feed to generate RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -149,5 +149,58 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) =>
+              allMarkdownRemark.edges.map(edge =>
+                Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: `${site.siteMetadata.siteUrl}/blog/${edge.node.frontmatter.slug}`,
+                  guid: `${site.siteMetadata.siteUrl}/blog/${edge.node.frontmatter.slug}`,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                }),
+              ),
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { frontmatter: { date: DESC } },
+                  filter: { fileAbsolutePath: { regex: "/content/posts/" }, frontmatter: { draft: { ne: true } } }
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      frontmatter {
+                        title
+                        date
+                        slug
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: 'Ehsanur Rahman Rhythm\'s Blog RSS Feed',
+          },
+        ],
+      },
+    },
   ],
 };
