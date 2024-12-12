eef43188bbb47a460942c8a1307c0d2dc571ed75
Ehsanur Rahman Rhythm <errhythm.me@gmail.com>
2024-12-12 12:33:39 +0600

    Add Clarity code to head element
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/errhythm/errhythm.github.io?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/components/head.js
+++ src/components/head.js
@@ -53,6 +53,16 @@ const Head = ({ title, description, image }) => {
       <meta name="twitter:image" content={seo.image} />
 
       <link rel="canonical" href={seo.url} />
+
+      <script type="text/javascript">
+        {`
+          (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "pcoam3db5l");
+        `}
+      </script>
     </Helmet>
   );
 };
