86bf1a5798b2e25d47b5eb30eb105cdf538fc81d
Ehsanur Rahman Rhythm <errhythm.me@gmail.com>
2025-01-15 16:43:44 +0600

    Refactor Google and Clarity tracking scripts in head.js for improved readability

--- src/components/head.js
+++ src/components/head.js
@@ -54,22 +54,22 @@ const Head = ({ title, description, image }) => {
 
       <link rel="canonical" href={seo.url} />
 
-      <!-- Google tag (gtag.js) -->
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y1XJH4ERHH"></script>
       <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-Y1XJH4ERHH');
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Y1XJH4ERHH');
+        `}
       </script>
 
       <script type="text/javascript">
         {`
           (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "pcoam3db5l");
         `}
       </script>
