19ac19ba64fff257320f195991736d57956c6ee5
Ehsanur Rahman Rhythm <errhythm.me@gmail.com>
2025-05-15 17:57:19 +0600

    Fix 404 routing issues for GitHub Pages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,11 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+
+// Handle redirects for SPA on GitHub Pages
+export const onRouteUpdate = () => {
+  const redirectMatch = window.location.search.match(/\?\/(.*)/);
+  if (redirectMatch) {
+    window.history.replaceState(null, null, `/${redirectMatch[1]}`);
+  }
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,33 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  
+  if (isProduction) {
+    setHeadComponents([
+      <script
+        key="spa-redirect"
+        dangerouslySetInnerHTML={{
+          __html: `
+            // Single Page Apps for GitHub Pages
+            // MIT License
+            // Based on https://github.com/rafgraph/spa-github-pages
+            (function(l) {
+              if (l.search[1] === '/') {
+                var decoded = l.search.slice(1).split('&').map(function(s) {
+                  return s.replace(/~and~/g, '&')
+                }).join('?');
+                window.history.replaceState(null, null,
+                  l.pathname.slice(0, -1) + decoded + l.hash
+                );
+              }
+            }(window.location))
+          `,
+        }}
+      />,
+    ]);
+  }
+};
